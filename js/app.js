eabcf43ea78ae43dfd265c776b740c5ecdeefb3d
zackforhack <zackforhack@gmail.com>
2015-03-07 00:32:07 -0500

    fix
    fix

--- js/app.js
+++ js/app.js
@@ -116,7 +116,21 @@
 
         $scope.saveItem = function(item){
         $scope.defaultItem = item;
-      }; 
+      };
+
+        function WeiXinShareBtn() {
+    if (typeof WeixinJSBridge == "undefined") {
+    alert(" 请先通过微信搜索 wow36kr 添加36氪为好友，通过微信分享文章 :) ");
+    } else {
+    WeixinJSBridge.invoke(‘shareTimeline‘, {
+    "title": "36氪",
+    "link": "http://www.36kr.com",
+    "desc": " 关注互联网创业 ",
+    "img_url": "http://www.36kr.com/assets/images/apple-touch-icon.png"
+    });
+    };
+
+    } 
 
     })
     .controller('OrderCtrl', function ($scope, $firebase, $rootScope, $animate){
